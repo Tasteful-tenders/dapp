0aa7864db65a4c44ed0e09ab64d0484112a0cfd7
Teddy <jfrancoisteddy@gmail.com>
2021-05-23 17:56:03 +0200

    Project initialization

---
+++ back/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+
+import { config } from "dotenv";
+config();
+
+import { buildRoutes } from "./routes";
+
+const app: Express = express();
+
+const path = require('path')
+const mongoose = require('mongoose')
+
+try {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+} catch (e) {
+    console.error(e)
+}
+
+app.use(express.json())
+
+buildRoutes(app)
+
+const port = 4003;
+app.listen(port, '0.0.0.0', function() {
+    console.log(`Listening on ${port}...`);
+});
