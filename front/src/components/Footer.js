ed251e63545115c24a92a4aecd67517f28aa860f
Jean-Francois Teddy <jfrancoisteddy@gmail.com>
2021-06-21 15:23:24 +0200

    feat : footer component

---
+++ front/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return(
+            <nav class="w-full font-all bg-black h-footer absolute bottom-0">
+            </nav>
+        );
+    }
+}
+
+export { Footer };
