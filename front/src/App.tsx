564ba8ebae3104053faca4a4a4f5927887291ed3
HHK <hhk.contact@protonmail.com>
2021-06-25 16:08:43 +0200

    feat: add navBar component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import {Header, Footer} from './components';
 import {Web3ReactProvider} from "@web3-react/core";
 import {Web3Provider} from "@ethersproject/providers";
 import ConnectModal from "./components/ConnectModal";
+import Route from "./components/Route";
 
 function getLibrary(provider: any) {
     const library = new Web3Provider(provider)
@@ -17,7 +18,12 @@ function App(): JSX.Element {
         <Web3ReactProvider getLibrary={getLibrary}>
             <div className="App">
                 <Header setOpen={setOpen}/>
-                {/* <Caroussel /> */}
+                <Route path={'/test'}>
+                    <h1>TEST</h1>
+                </Route>
+                <Route path={'/test2'}>
+                    <h1>TEST2</h1>
+                </Route>
                 <ConnectModal open={open} setOpen={setOpen}/>
                 <Footer/>
             </div>
--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -9,11 +9,11 @@ import {Web3Provider} from "@ethersproject/providers";
 import {useWeb3React} from "@web3-react/core";
 import {ContractHelper} from "../contractHelper";
 import {providers} from "ethers";
+import Link from "./Link";
 
-export function Header({setOpen}: {setOpen: Function}) {
+export function Header({setOpen}: { setOpen: Function }): JSX.Element {
     const context = useWeb3React<Web3Provider>();
-    const { connector, library, chainId, account, activate, deactivate, active, error } = context;
-    const connectBtnLabel = active && account ? account?.slice(0, 5)+'***'+account?.slice(account?.length - 5, account?.length - 1) : 'Connect wallet';
+    const {connector, library, chainId, account, activate, deactivate, active, error} = context;
 
     useEffect(() => {
         async function init() {
@@ -22,15 +22,16 @@ export function Header({setOpen}: {setOpen: Function}) {
                 ContractHelper.init(web3Provider);
             }
         }
+
         init();
     }, [active]);
 
     return (
         <nav className="flex items-center justify-between flex-wrap w-full px-32 py-5 font-all">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <a href="">
+                <Link className={''} href={'/'}>
                     <img src={logo} alt="logo" className=""/>
-                </a>
+                </Link>
             </div>
             <div className="bg-white flex items-center rounded-full shadow-md border-rounded w-search_bar">
                 <input type="text" placeholder="search..."
@@ -39,14 +40,35 @@ export function Header({setOpen}: {setOpen: Function}) {
                     <FontAwesomeIcon icon={faSearch} className="w-icon text-grey"/>
                 </div>
             </div>
-            <div className="flex  flex-shrink-0">
-                <button
-                    className="bg-black text-white px-9 focus:outline-none border-rounded font-medium text-medium"
-                    onClick={() => setOpen(true)}
-                >{connectBtnLabel}
-                </button>
+            <div className="flex flex-shrink-0">
+                <NavBar setOpen={setOpen}/>
                 <img src={profile_icon} alt="profile_icon" className="h-xl pl-5"/>
             </div>
         </nav>
     );
+}
+
+function NavBar({setOpen}: { setOpen: Function }): JSX.Element {
+    const context = useWeb3React<Web3Provider>();
+    const {connector, library, chainId, account, activate, deactivate, active, error} = context;
+    const connectBtnLabel = active && account ? account?.slice(0, 5) + '***' + account?.slice(account?.length - 5, account?.length - 1) : 'Connect wallet';
+
+    if (active) {
+        return (
+            <>
+                <Link className={'bg-black text-white px-9 focus:outline-none border-rounded font-medium text-medium'} href={'/test'}>
+                    test
+                </Link>
+                <Link className={'bg-black text-white px-9 focus:outline-none border-rounded font-medium text-medium'} href={'/test2'}>
+                    test2
+                </Link>
+            </>
+        );
+    }
+    return (
+        <button
+            className="bg-black text-white px-9 focus:outline-none border-rounded font-medium text-medium"
+            onClick={() => setOpen(true)}
+        >{connectBtnLabel}
+        </button>);
 }
