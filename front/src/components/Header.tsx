4e6219c86f23a59a8147f3f2b86a3678b76a5805
Jean-Francois Teddy <jfrancoisteddy@gmail.com>
2021-07-04 13:17:36 +0200

    fix: center header elements

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -62,10 +62,10 @@ function NavBar({setOpen}: { setOpen: Function }): JSX.Element {
     if (active) {
         return (
             <>
-                <Link className={'px-9 font-medium text-medium'} href={'/bids'}>
+                <Link className={'px-9 font-medium text-medium flex items-center'} href={'/bids'}>
                     BIDS
                 </Link>
-                <Link className={'px-9 font-medium text-medium'} href={'/mint'}>
+                <Link className={'px-9 font-medium text-medium flex items-center'} href={'/mint'}>
                     MINT
                 </Link>
             </>
