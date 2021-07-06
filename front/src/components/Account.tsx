2513224d7760e75a1363b1d7c749e741df04e985
HHK <hhk.contact@protonmail.com>
2021-07-06 11:17:27 +0200

    feat: Display userNfts on his accountpage

--- front/src/components/Account.tsx
+++ front/src/components/Account.tsx
@@ -1,8 +1,9 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext} from "react";
 import {useWeb3React} from "@web3-react/core";
 import {Web3Provider} from "@ethersproject/providers";
-import {ContractHelper, INFTData} from "../contractHelper";
+import {INFTData} from "../contractHelper";
 import {TastefulData} from "../context";
+import {TenderCard} from "./TenderCard";
 
 export function Account(): JSX.Element {
     const web3Context = useWeb3React<Web3Provider>();
@@ -20,22 +21,18 @@ export function Account(): JSX.Element {
 
     return (
         <div>
-            <h1>Created</h1>
-            {userNfts && userNfts.created.map((nft: INFTData, index) => {
-                return (
-                    <div key={index}>
-                        {nft.id}
-                    </div>
-                );
-            })}
-            <h1>Owned</h1>
-            {userNfts && userNfts.owned.map((nft: INFTData, index) => {
-                return (
-                    <div key={index}>
-                        {nft.id}
-                    </div>
-                );
-            })}
+            <h1>Created :</h1>
+            <div className={"grid grid-cols-5 gap-4"}>
+                {userNfts && userNfts.created.map((data: INFTData, index) => {
+                    return <TenderCard nftData={data} tender={tenders[data.id.toNumber()-1]} key={index}/>
+                })}
+            </div>
+            <h1>Owned :</h1>
+            <div>
+                {userNfts && userNfts.owned.map((data: INFTData, index) => {
+                    return <TenderCard nftData={data} tender={tenders[data.id.toNumber()-1]} key={index}/>
+                })}
+            </div>
         </div>
     );
 }
