73be5bfb1f7ea7950793bde9fcf3288ead857a5d
HHK <hhk.contact@protonmail.com>
2021-07-07 00:36:12 +0200

    feat: display userData in account component

--- front/src/components/Account.tsx
+++ front/src/components/Account.tsx
@@ -11,7 +11,7 @@ export function Account(): JSX.Element {
     const web3Context = useWeb3React<Web3Provider>();
     const {connector, library, chainId, account, activate, deactivate, active, error} = web3Context;
     const tastefulContext = useContext(TastefulData);
-    const { tenders, nftsData, userNfts } = tastefulContext;
+    const { tenders, nftsData, userNfts, userData } = tastefulContext;
 
     if (!active) {
         return (
@@ -25,26 +25,22 @@ export function Account(): JSX.Element {
         <div className={"grid grid-cols-3 m-2"}>
             <div className={"p-12"}>
                 <img className={"m-auto border-nft-card"} src={"https://via.placeholder.com/250"} alt={"img"}/>
-                <h3 className={"text-medium font-black break-words my-8"}>{account}</h3>
+                <h3 className={"text-medium font-black break-words my-8"}>{userData.pseudo !== undefined ? userData.pseudo : account}</h3>
                 <div className={"mt-32"}>
-                    <a className={"block mb-4"} href={"https://twitter.com/hhk_eth"} target={"_blank"}>
+                    <a className={"block mb-4"} href={"https://twitter.com/"+userData.twitter} target={"_blank"}>
                         <img className={"h-logo inline-block mr-12"} src={twitter} alt={'logo'}/>
-                        <h3 className={"inline-block text-medium"}>@HHK_ETH</h3>
+                        <h3 className={"inline-block text-medium"}>{userData.twitter !== undefined ? '@'+userData.twitter : 'No account'}</h3>
                     </a>
-                    <a className={"block"} href={"https://instagram.com/hhk_eth"} target={"_blank"}>
+                    <a className={"block"} href={"https://instagram.com/"+userData.instagram} target={"_blank"}>
                         <img className={"h-logo inline-block mr-12 -mt-2"} src={instagram} alt={'logo'}/>
-                        <h3 className={"inline-block text-medium"}>@HHK_ETH</h3>
+                        <h3 className={"inline-block text-medium"}>{userData.instagram !== undefined ? '@'+userData.instagram : 'No account'}</h3>
                     </a>
                 </div>
             </div>
             <div className={"col-span-2"}>
                 <div className={"w-two_third mb-8"}>
                     <h3 className={"text-large font-black mb-4"}>BIO</h3>
-                    <p>Use the col-start-n and col-end-n utilities to make an element start or end at the nth grid line.
-                        These can also be combined with the col-span-n utilities to span a specific number of columns.
-
-                        Note that CSS grid lines start at 1, not 0,
-                        so a full-width element in a 6-column grid would start at line 1 and end at line 7.</p>
+                    <p>{userData.bio}</p>
                 </div>
                 <div>
                     <div className={"grid grid-cols-8"}>
