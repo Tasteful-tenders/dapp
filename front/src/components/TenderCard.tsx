a9cb7da05e425c8c067c81cb144883dc1f28be8f
HHK <hhk.contact@protonmail.com>
2021-07-06 11:59:51 +0200

    feat: improve tendercards and home css

--- front/src/components/TenderCard.tsx
+++ front/src/components/TenderCard.tsx
@@ -12,16 +12,16 @@ export function TenderCard({nftData, tender}: { nftData: INFTData, tender: ITend
             <div className={"w-nft_card relative font-all"}>
                 <Link to={"/nftHomepage/"+nftData.id}>
                 <div
-                    className={" bg-white w-full h-top_card grid justify-items-center border-top-nft-card shadow-xl top-0"}>
+                    className={"bg-white w-full h-top_card grid justify-items-center border-top-nft-card shadow-xl top-0"}>
                     <h3 className="flex items-center text-large font-black">{nftData.title}</h3>
                     <h3 className="flex items-center text-card font-light text-grey">by {nftData.author}</h3>
                 </div>
-                <div className={"border-nft-card shadow-xl"}>
+                <div className={"shadow-xl h-image_card"}>
                     <img src={'https://ipfs.io/ipfs/' + nftData.ipfsHash} alt={'image'}
-                         className="border-nft-card shadow-xl"/>
+                         className="shadow-xl transform max-h-image_card m-auto"/>
                 </div>
                 <div
-                    className={" bg-black text-white grid grid-cols-2 h-bottom_card font-black border-bottom-nft-card shadow-xl bottom-0 w-full"}>
+                    className={"bg-black text-white grid grid-cols-2 h-bottom_card font-black border-bottom-nft-card shadow-xl bottom-0 w-full"}>
                     <div className="col-start-1 pt-4">
                         <h3 className="grid justify-items-center text-small">Current bid</h3>
                         <h3 className="grid justify-items-center text-medium">{currentBid.toNumber()} TTK</h3>
